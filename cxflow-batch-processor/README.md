ebbb05b83e45d8c09d87eabd07b48a13e7865506
Miguel <miguelagonzalez02@gmail.com>
2021-10-29 16:27:17 -0500

    Adding readme

---
+++ cxflow-batch-processor/README.md
@@ -0,0 +1,30 @@
+# CxFlow Batch Processor
+This script will take an input of a project mapping csv and create tickets in batch mode for your Checkmarx projects. The project mapping will contain fields that will allow you to use different yml files to point to different Jira or bug tracker instances or you can also use environment variable to provide these values. 
+
+## Environment Variables
+If you would like to use environment variables to provide the cxflow parameters, you will need to follow the format below. When using Environment Variables, all 3 environment variables need to be provided. *Please note this is all case sensivite*
+
+Environment Variable | Variable Name Example | Value Example
+---------------------|-----------------------|--------------
+{Bug_Tracker}_url_{Bug_Tracker_Instance} |  Jira_url_cxmgl, github_url_cloudInstance | https://api.github.com/repos/
+{Bug_Tracker}_token_{Bug_Tracker_Instance} | Jira_token_cxmgl, github_token_OnPrem | token1234
+${Bug_Tracker}_user_${Bug_Tracker_Instance} | Jira_user_cxmgl, azure_user_CxAzure | user@email.com
+--------------------|------------------------|---------------
+
+- Note the {Bug_Tracker} and {Bug_Tracker_Instance} need to match the values from your projectMap.csv file 
+
+Project Map Columns | Description
+--------------------|------------
+Checkmarx_Project | Project name as seen in Checkmarx
+Checkmarx_Team | The Checkmarx team that the given project belongs to
+Bug_Tracker | Jira, GitLab, GitHub, Azure
+Bug_Tracker_Instance | A labe for the given Bug_Track ex. OnPrem, Cloud, etc
+Jira_Project * | Jira project key
+Jira_Issue_Type * | issue type for ticket that will be opened 
+Repo_Name ** | Name of repository
+Branch ** | Repository branch
+Namespace ** | Namespace for the repo/branch provided
+Config | the relative file path to the yml file that you want to use for cxflow
+
+"*" - Denotes this field is required if Bug_Tracker = Jira
+"**" - Denotes this field is required if Bug_Tracker = GitLab, GitHub, Azure
--- cxflow-batch-processor/batchProcessor.sh
+++ cxflow-batch-processor/batchProcessor.sh
@@ -20,12 +20,14 @@ fi
 echo "Validating mapping file projectMap.csv"
 #set the count
 count=0
-while IFS==, read -r Checkmarx_Project Checkmarx_Team Bug_Tracker Jira_Instance Jira_Project Jira_Issue_Type Repo_Name Branch Namespace Config end; do
+while IFS==, read -r Checkmarx_Project Checkmarx_Team Bug_Tracker Jira_Instance Jira_Project Jira_Issue_Type SCM_Instance Repo_Name Branch Namespace Config end; do
     #check headers
     echo $Checkmarx_Project
     if [[ $count == 0 ]]
     then 
-        if [[ $Checkmarx_Project == "Checkmarx_Project" && $Checkmarx_Team == "Checkmarx_Team" && $Bug_Tracker == "Bug_Tracker" && $Jira_Instance == "Jira_Instance" && $Jira_Project == "Jira_Project" && $Jira_Issue_Type == "Jira_Issue_Type" && $Repo_Name == "Repo_Name" && $Branch == "Branch" && $Namespace == "Namespace" && $Config == "Config" ]]
+        if [[ $Checkmarx_Project == "Checkmarx_Project" && $Checkmarx_Team == "Checkmarx_Team" && $Bug_Tracker == "Bug_Tracker" 
+            && $Bug_Tracker_Instance == "Bug_Tracker_Instance" && $Jira_Project == "Jira_Project" && $Jira_Issue_Type == "Jira_Issue_Type" 
+            && $SCM_Instance == "SCM_Instance" && $Repo_Name == "Repo_Name" && $Branch == "Branch" && $Namespace == "Namespace" && $Config == "Config" ]]
         then
             echo "Headers successfully validated"
         else
@@ -37,24 +39,28 @@ while IFS==, read -r Checkmarx_Project Checkmarx_Team Bug_Tracker Jira_Instance 
         if [[ $Bug_Tracker == "Jira" ]]
         then
         #grab the correct Jira credentials
-        jira_url=jira_url_$Jira_Instance
-        jira_token=jira_token_$Jira_Instance
-        jira_user=jira_user_$Jira_Instance
+        bug_tracker_url=jira_url_$Bug_Tracker_Instance
+        bug_tracker_token=jira_token_$Bug_Tracker_Instance
+        bug_tracker_user=jira_user_$Bug_Tracker_Instance
         #add check to see if environment variables exist
-            if [[ ! -z $jira_url || ! -z $jira_token || ! -z $jira_user ]]
+            if [[ ! -z $bug_tracker_url || ! -z $bug_tracker_token || ! -z $bug_tracker_user ]]
             then
-                echo "No environment variables set for $Jira_Instance"
-                echo "java -jar $cxflowjar --project --cx-team=$Checkmarx_Team --cx-project=$Checkmarx_Project --bug-tracker=$Bug_Tracker --app=$Jira_Project --jira.project=$Jira_Project --jira.issuetype=$Jira_Issue_Type --spring.config.location=$Config"
-                nohup java -jar $cxflowjar --project --cx-team=$Checkmarx_Team --cx-project=$Checkmarx_Project --bug-tracker=$Bug_Tracker --app=$Jira_Project --jira.project=$Jira_Project --jira.issuetype=$Jira_Issue_Type --spring.config.location=$Config > $filename
+            #use environment variables
+                echo "environment variables set for $Jira_Instance"
+                #echo "java -jar $cxflowjar --project --cx-team=$Checkmarx_Team --cx-project=$Checkmarx_Project --bug-tracker=$Bug_Tracker --app=$Jira_Project --jira.project=$Jira_Project --jira.issuetype=$Jira_Issue_Type --spring.config.location=$Config"
+                echo "java -jar $cxflowjar --project --cx-team=$Checkmarx_Team --cx-project=$Checkmarx_Project --bug-tracker=$Bug_Tracker --bug-tracker-impl=$Bug_Tracker --app=$Checkmarx_Project --jira.project=$Jira_Project --jira.issuetype=$Jira_Issue_Type --branch=$Branch --repo-name=$repo_Name --namespace=$Namespace --$Bug_tracker.url=$bug_tracker_url --$Bug_tracker.token=$bug_tracker_token --$Bug_tracker.username=$bug_tracker_user --spring.config.location=$Config"
+                nohup java -jar $cxflowjar --project --cx-team=$Checkmarx_Team --cx-project=$Checkmarx_Project --bug-tracker=$Bug_Tracker --bug-tracker-impl=$Bug_Tracker --app=$Checkmarx_Project --jira.project=$Jira_Project --jira.issuetype=$Jira_Issue_Type --branch=$Branch --repo-name=$repo_Name --namespace=$Namespace --$Bug_tracker.url=$bug_tracker_url --$Bug_tracker.token=$bug_tracker_token --$Bug_tracker.username=$bug_tracker_user --spring.config.location=$Config > $filename
             else
+            #pull everything from config
                 #run the batch command
-                echo "java -jar $cxflowjar --project --cx-team=$Checkmarx_Team --cx-project=$Checkmarx_Project --bug-tracker=$Bug_Tracker --app=$Jira_Project --jira.project=$Jira_Project --jira.issuetype=$Jira_Issue_Type --spring.config.location=$Config"
-                nohup java -jar $cxflowjar --project --cx-team=$Checkmarx_Team --cx-project=$Checkmarx_Project --bug-tracker=$Bug_Tracker --app=$Jira_Project --jira.project=$Jira_Project --jira.issuetype=$Jira_Issue_Type --jira.url=$jira_url --jira.username=$jira_user --jira.token=$jira_token --spring.config.location=$Config > $filename
+                echo "environment variables set for $Jira_Instance"
+                echo "java -jar $cxflowjar --project --cx-team=$Checkmarx_Team --cx-project=$Checkmarx_Project --bug-tracker=$Bug_Tracker --bug-tracker-impl=$Bug_Tracker --app=$Checkmarx_Project --jira.project=$Jira_Project --jira.issuetype=$Jira_Issue_Type --branch=$Branch --repo-name=$repo_Name --namespace=$Namespace --spring.config.location=$Config"
+                nohup java -jar $cxflowjar --project --cx-team=$Checkmarx_Team --cx-project=$Checkmarx_Project --bug-tracker=$Bug_Tracker --bug-tracker-impl=$Bug_Tracker --app=$Checkmarx_Project --jira.project=$Jira_Project --jira.issuetype=$Jira_Issue_Type --branch=$Branch --repo-name=$repo_Name --namespace=$Namespace --spring.config.location=$Config > $filename
             fi
-        else
-        echo "java -jar $cxflowjar --project --cx-team=$Checkmarx_Team --cx-project=$Checkmarx_Project --bug-tracker=$Bug_Tracker --bug-tracker-impl=$Bug_Tracker --branch=$Branch --repo-name=$repo_Name --namespace=$Namespace --app=$Checkmarx_Project --jira.project=$Jira_Project --jira.issuetype=$Jira_Issue_Type --jira.token=$JIRA1_TOKEN --spring.config.location=$Config"
-        nohup java -jar $cxflowjar --project --cx-team=$Checkmarx_Team --cx-project=$Checkmarx_Project --bug-tracker=$Bug_Tracker --bug-tracker-impl=$Bug_Tracker --branch=$Branch --repo-name=$Repo_Name --namespace=$Namespace --app=$Checkmarx_Project --jira.project=$Jira_Project --jira.issuetype=$Jira_Issue_Type --jira.token=$JIRA1_TOKEN --spring.config.location=$Config > $filename
-        fi
+#        else
+#        echo "java -jar $cxflowjar --project --cx-team=$Checkmarx_Team --cx-project=$Checkmarx_Project --bug-tracker=$Bug_Tracker --bug-tracker-impl=$Bug_Tracker --branch=$Branch --repo-name=$repo_Name --namespace=$Namespace --app=$Checkmarx_Project --spring.config.location=$Config"
+#        nohup java -jar $cxflowjar --project --cx-team=$Checkmarx_Team --cx-project=$Checkmarx_Project --bug-tracker=$Bug_Tracker --bug-tracker-impl=$Bug_Tracker --branch=$Branch --repo-name=$Repo_Name --namespace=$Namespace --app=$Checkmarx_Project --spring.config.location=$Config > $filename
+#        fi
     fi
     ((++count))
 done < $map
