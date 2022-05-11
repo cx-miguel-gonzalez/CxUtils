95563d72bb3c1ef623a3c8db38f746b21d31d8a8
mgonzalezcx <48724903+mgonzalezcx@users.noreply.github.com>
2022-05-11 16:36:52 -0500

    Updated branched project cleanup logic and improved csv

--- cx-sast-shell-tools/SAST-BranchedProject-Cleanup.ps1
+++ cx-sast-shell-tools/SAST-BranchedProject-Cleanup.ps1
@@ -3,6 +3,8 @@ param(
     [System.Uri]$sast_url,
     [String]$username,
     [String]$password,
+    [Parameter(Mandatory = $true)]
+    [String]$cutoff,
     [Switch]$dbg
 )
 
@@ -16,6 +18,11 @@ if(!$username){
 
 setupDebug($dbg.IsPresent)
 
+#convert cutoff date
+$culture = [Globalization.CultureInfo]::InvariantCulture
+$pattern = "MM\/dd\/yyyy"
+$cutOffDate = [DateTime]::ParseExact($cutoff, $pattern, $culture)
+
 #Login and generate token
 $session = &"support/rest/sast/login.ps1" $sast_url $username $password -dbg:$dbg.IsPresent
 
@@ -24,7 +31,7 @@ $allprojects = &"support/rest/sast/projects.ps1" $session "2.2"
 $teams = &"support/rest/sast/teams.ps1" $session
 
 
-$csvDetails = @()
+$targetProjects = @()
 
 #branched projects
 $branchedProjects = $allprojects | Where-Object {$_.isBranched -eq 1}
@@ -36,48 +43,76 @@ $branchedProjects | %{
     $prjName = $_.Name
     $prjTeamId = $_.teamId
     $prjTeam = $teams | Where-Object {$_.id -eq $prjTeamId}
+
     #parent project details
     $parentProjectId = $_.originalProjectId
     $parentProject = $allprojects | Where-Object {$_.id -eq $parentProjectId}
     $parentTeam = $teams | Where-Object {$_.id -eq $parentProject.teamId}
 
-    try{
-        $scmSettings = &"support/rest/sast/getprojectgitdetails.ps1" $session $prjId
+    try {
+        $lastScan = &"support/rest/sast/scans.ps1" $session $prjId
+        $scanDate = Get-Date($lastScan.dateAndTime.finishedOn)
+        Write-Debug $scanDate
         
-        $csvEntry = New-Object -TypeName psobject -Property ([Ordered]@{
-            projectId = $prjId;
-            projectName = $prjName;
-            owningTeam = $prjTeam.Name;
-            projectUrl = $scmSettings.url;
-            projectGitBranch = $scmSettings.branch;
-            parentProject = $parentProject.Name
-            parentTeam = $parentTeam.Name
-        })
-        
-        
-        Write-Debug $csvEntry
-        $csvDetails +=$csvEntry
+        if($scanDate -lt $cutOffDate){        
+            try{
+                $scmSettings = &"support/rest/sast/getprojectgitdetails.ps1" $session $prjId
+            }
+            catch{
+                Write-Debug "This project: $prjName , does not have git configuration"
+                $scmSettings = @{
+                    url = "None"
+                    Branch = "None"
+                }
+            }
+        }
     }
-    catch{
-        Write-Debug "This project: $prjName , does not have git configuration"
+    catch {
+        Write-Debug "This project has not been scanned"
+        $scmSettings = @{
+            url = "None"
+            branch = "None"
+        }
+        $scanDate = "Never Scanned"
     }
+    
+    
+    $csvEntry = New-Object -TypeName psobject -Property ([Ordered]@{
+        projectId = $prjId;
+        projectName = $prjName;
+        lastScanDate = $scanDate
+        owningTeam = $prjTeam.Name;
+        projectUrl = $scmSettings.url;
+        projectGitBranch = $scmSettings.branch;
+        parentProject = $parentProject.Name
+        parentTeam = $parentTeam.Name
+    })
+    
+    
+    #Write-Debug $csvEntry
+    $targetProjects +=$csvEntry
+       
 }
 
-$csvDetails | Export-Csv -Path './BranchedProjectDetails.csv' -Delimiter ',' -Append -NoTypeInformation
+$targetProjects | Export-Csv -Path './TargetBranchedProjectDetails.csv' -Delimiter ',' -Append -NoTypeInformation
 
-Write-Output "Please review the csv and verify that you wish to delete all projects found in the BranchedProjectDetails.csv"
+Write-Output "Please review the csv and verify that you wish to delete all projects found in the TargetBranchedProjectDetails.csv"
 $verification = Read-Host -Prompt "Do you wish to delete all of the listed projects (y/n)"
 
 $failedProjects = @()
 if($verification -eq 'y'){
-    $branchedProjects | %{
+    $targetProjects | %{
         try{
-            &"support/rest/sast/deleteTeam.ps1" $session $_.id
+            &"support/rest/sast/deleteproject.ps1" $session $_.projectId
         }
         catch{
             Write-Debug "this one failed"
             $failedProjects += $_
         }
     }
+    $failedProjects | Export-Csv -Path './FailedToDeleteProjects.csv' -Delimiter ',' -Append -NoTypeInformation
 }
-$failedProjects | Export-Csv -Path './FailedToDeleteProjects.csv' -Delimiter ',' -Append -NoTypeInformation
+else{
+    Write-Output "This process has been aborted"
+    Exit
+}
